07ab709f59306b8b047d463f4e49ea3b8350ddb2
Abobora <lucasclucas7@gmail.com>
2023-01-30 17:04:08 -0300

    Projeto finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,16 @@
 <template>
+  <header>
+    <h1>Goodbye Boredom</h1>
+  </header>
   <div id="content">
-    <idea-component
-      activity="Contribute code or a monetary donation to an open-source software project"
-      type="charity"
-      participants="1"
-      price="0.1"
-      link="https://github.com/explore"
-      accessibility="0"
+    <idea-component v-for="(idea, index) in response" :key="index"
+      :activity="idea.activity"
+      :participants="idea.participants"
+      :link="idea.link"
     />
-    <idea-component
-      activity="Contribute code or a monetary donation to an open-source software project"
-      type="charity"
-      participants="1"
-      price="0.1"
-      link="https://github.com/explore"
-      accessibility="0"
-    />
-    <idea-component
-      activity="Contribute code or a monetary donation to an open-source software project"
-      type="charity"
-      participants="1"
-      price="0.1"
-      link="https://github.com/explore"
-      accessibility="0"
-    />
-    <idea-component
-      activity="Contribute code or a monetary donation to an open-source software project"
-      type="charity"
-      participants="1"
-      price="0.1"
-      link="https://github.com/explore"
-      accessibility="0"
-    />
+    <div id="loadMoreContainer">
+      <button @click="getMoreIdeas">Load More...</button>
+    </div>
   </div>
 </template>
 
@@ -47,7 +26,7 @@ export default {
   },
   methods: {
     getMoreIdeas() {
-      for (let i = 0; i <= 30; i++) {
+      for (let i = 1; i <= 30; i++) {
         axios("https://www.boredapi.com/api/activity/").then((response) =>
           this.response.push(response.data)
         );
@@ -68,16 +47,55 @@ export default {
   padding: 0px;
   outline: 0px;
   font-family: 'Montserrat', sans-serif;
+  color: rgba(245, 245, 245, 0.79);
+}
+
+header {
+  background-color: #1e1e25;
+  border-bottom: 2px solid #14141b;
+}
+
+header h1 {
+  padding: 10px 30px;
 }
 
 body {
-  background-color: #242424;
+  background-color: #23232a;
+  overflow-x: hidden;
 }
 
 #content {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
+}
+
+#loadMoreContainer {
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+  margin: 30px 0px;
+}
+
+#loadMoreContainer > button {
+    background-color: #2b2b32;
+    border: 1px solid black;
+    padding: 7px 30px;
+    border-radius: 5px;
+    cursor: pointer;
+    box-shadow: 2px 2px 3px #0000005f;
+    font-weight: bold;
+    transition: 500ms;
+}
+
+#loadMoreContainer > button:hover {
+  transform: scale(1.2);
+}
+
+@media screen and (max-width: 375px) {
+  header > h1 {
+    font-size: 100%;
+  }
 }
 
 </style>
--- src/components/IdeaComponent.vue
+++ src/components/IdeaComponent.vue
@@ -1,21 +1,15 @@
 <template>
   <article>
     <h1 id="activity">{{ activity }}</h1>
-
-    <p id="price">{{ price }}</p>
-    <p id="participants">{{ participants }}</p>
-    <p id="type">{{ type }}</p>
-
+    <span id="participants">Participants: {{ participants }}</span>
     <a v-if="link" :href="link" id="link">Link</a>
-
-    <p id="accessibility">{{ accessibility }}</p>
   </article>
 </template>
 
 <script>
 export default {
     name: 'IdeaComponent',
-    props: [ 'activity', 'price', 'type', 'participants', 'link', 'accessibility' ]
+    props: [ 'activity', 'participants', 'link']
 };
 </script>
 
@@ -26,12 +20,46 @@ article {
     width: 25vw;
     margin: 10px;
     padding: 20px 15px;
-    border: 1px solid black;
+    border: 1px solid rgba(0, 0, 0, 0.692);
+    background-color: #2f2f39;
+    border-radius: 10px;
+    box-shadow: 3px 3px 8px #00000094;
+    transition: 800ms;
+}
+
+article:hover {
+  transform: scale(1.08);
 }
 
 article > h1 {
+    width: 100%;
     text-align: center;
-    color: rgba(245, 245, 245, 0.79);
     margin: 20px 0px;
 }
+
+article > #participants {
+  /* display: block; */
+  width: 100%;
+  text-align: center;
+}
+
+article a {
+  width: 100%;
+  text-align: end;
+  margin: 10px 1vw;
+}
+
+@media screen and (max-width: 1100px) {
+  article {
+    width: 40vw;
+  }
+}
+
+@media screen and (max-width: 630px) {
+  article {
+    width: 90vw;
+    font-size: .9em;
+  }
+}
+
 </style>
