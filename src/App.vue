309392d02c3d3fe599390e7330a694caa9af23da
Abobora <lucasclucas7@gmail.com>
2023-01-29 23:30:45 -0300

    Projeto parcialmente finalizado

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="content">
+    <idea-component
+      activity="Contribute code or a monetary donation to an open-source software project"
+      type="charity"
+      participants="1"
+      price="0.1"
+      link="https://github.com/explore"
+      accessibility="0"
+    />
+    <idea-component
+      activity="Contribute code or a monetary donation to an open-source software project"
+      type="charity"
+      participants="1"
+      price="0.1"
+      link="https://github.com/explore"
+      accessibility="0"
+    />
+    <idea-component
+      activity="Contribute code or a monetary donation to an open-source software project"
+      type="charity"
+      participants="1"
+      price="0.1"
+      link="https://github.com/explore"
+      accessibility="0"
+    />
+    <idea-component
+      activity="Contribute code or a monetary donation to an open-source software project"
+      type="charity"
+      participants="1"
+      price="0.1"
+      link="https://github.com/explore"
+      accessibility="0"
+    />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import IdeaComponent from "./components/IdeaComponent.vue";
+
+export default {
+  name: "App",
+  components: { IdeaComponent },
+  data() {
+    return { response: [] };
+  },
+  methods: {
+    getMoreIdeas() {
+      for (let i = 0; i <= 30; i++) {
+        axios("https://www.boredapi.com/api/activity/").then((response) =>
+          this.response.push(response.data)
+        );
+      }
+    },
+  },
+  mounted() {
+    this.getMoreIdeas();
+  },
+};
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+* {
+  margin: 0px;
+  padding: 0px;
+  outline: 0px;
+  font-family: 'Montserrat', sans-serif;
+}
+
+body {
+  background-color: #242424;
+}
+
+#content {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+</style>
---
+++ src/components/IdeaComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <article>
+    <h1 id="activity">{{ activity }}</h1>
+
+    <p id="price">{{ price }}</p>
+    <p id="participants">{{ participants }}</p>
+    <p id="type">{{ type }}</p>
+
+    <a v-if="link" :href="link" id="link">Link</a>
+
+    <p id="accessibility">{{ accessibility }}</p>
+  </article>
+</template>
+
+<script>
+export default {
+    name: 'IdeaComponent',
+    props: [ 'activity', 'price', 'type', 'participants', 'link', 'accessibility' ]
+};
+</script>
+
+<style scoped>
+article {
+    display: flex;
+    flex-wrap: wrap;
+    width: 25vw;
+    margin: 10px;
+    padding: 20px 15px;
+    border: 1px solid black;
+}
+
+article > h1 {
+    text-align: center;
+    color: rgba(245, 245, 245, 0.79);
+    margin: 20px 0px;
+}
+</style>
